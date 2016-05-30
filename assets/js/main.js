2b4f0fa3352638e466e75fe295e84f975814a1ee
Dalimil <dalimilhajek@gmail.com>
2016-05-30 21:44:35 +0100

    Cleanup code

--- assets/js/main.js
+++ assets/js/main.js
@@ -27,6 +27,70 @@ $(document).ready(function () {
 		return false;
 	});
 
+	var settings = {
+		parallax: true, // Parallax background effect?
+		parallaxFactor: 90 // Parallax factor (lower = more intense, higher = less intense).
+	};
+
+	skel.breakpoints({
+		xlarge: '(max-width: 1800px)',
+		large: '(max-width: 1280px)',
+		medium: '(max-width: 980px)',
+		small: '(max-width: 736px)',
+		xsmall: '(max-width: 480px)'
+	});
+
+	$(function() {
+		var $window = $(window),
+			$body = $('body'),
+			$header = $('#header');
+
+		// Disable animations/transitions until the page has loaded.
+		$body.addClass('is-loading');
+
+		$window.on('load', function() {
+			$body.removeClass('is-loading');
+		});
+
+		// Touch?
+		if (skel.vars.mobile) {
+			// Turn on touch mode.
+			$body.addClass('is-touch');
+
+			// Height fix (mostly for iOS).
+			window.setTimeout(function() {
+				$window.scrollTop($window.scrollTop() + 1);
+			}, 0);
+
+		}
+
+		// Header - Parallax background.
+
+		// Disable parallax on IE (smooth scrolling is jerky), and on mobile platforms (= better performance).
+		if (skel.vars.browser == 'ie' || skel.vars.mobile) {
+			settings.parallax = false;
+		}
+
+		if (settings.parallax) {
+			skel.on('change', function() {
+
+				if (skel.breakpoint('medium').active) {
+					$window.off('scroll.strata_parallax');
+					$header.css('background-position', 'top left, center center');
+				}
+				else {
+					$header.css('background-position', 'left 0px');
+					$window.on('scroll.strata_parallax', function() {
+						$header.css('background-position', 'left ' + (-1 * (parseInt($window.scrollTop()) / settings.parallaxFactor)) + 'px');
+					});
+				}
+
+			});
+		}
+
+	});
+
+	// Typing animation
 	if(skel.vars.mobile) {
 		$("#intro-header").hide();
 	} else {
@@ -42,87 +106,5 @@ $(document).ready(function () {
 			// Callback can be specified - see https://github.com/mattboldt/typed.js
 		});
 	}
+
 });
-
-(function($) {
-
-	var settings = {
-
-		// Parallax background effect?
-			parallax: true,
-
-		// Parallax factor (lower = more intense, higher = less intense).
-			parallaxFactor: 90
-
-	};
-
-	skel.breakpoints({
-		xlarge: '(max-width: 1800px)',
-		large: '(max-width: 1280px)',
-		medium: '(max-width: 980px)',
-		small: '(max-width: 736px)',
-		xsmall: '(max-width: 480px)'
-	});
-
-	$(function() {
-
-		var $window = $(window),
-			$body = $('body'),
-			$header = $('#header');
-
-		// Disable animations/transitions until the page has loaded.
-			$body.addClass('is-loading');
-
-			$window.on('load', function() {
-				$body.removeClass('is-loading');
-			});
-
-		// Touch?
-			if (skel.vars.mobile) {
-
-				// Turn on touch mode.
-					$body.addClass('is-touch');
-
-				// Height fix (mostly for iOS).
-					window.setTimeout(function() {
-						$window.scrollTop($window.scrollTop() + 1);
-					}, 0);
-
-			}
-
-		// Header.
-
-			// Parallax background.
-
-				// Disable parallax on IE (smooth scrolling is jerky), and on mobile platforms (= better performance).
-					if (skel.vars.browser == 'ie'
-					||	skel.vars.mobile)
-						settings.parallax = false;
-
-				if (settings.parallax) {
-
-					skel.on('change', function() {
-
-						if (skel.breakpoint('medium').active) {
-
-							$window.off('scroll.strata_parallax');
-							$header.css('background-position', 'top left, center center');
-
-						}
-						else {
-
-							$header.css('background-position', 'left 0px');
-
-							$window.on('scroll.strata_parallax', function() {
-								$header.css('background-position', 'left ' + (-1 * (parseInt($window.scrollTop()) / settings.parallaxFactor)) + 'px');
-							});
-
-						}
-
-					});
-
-				}
-
-	});
-
-})(jQuery);
