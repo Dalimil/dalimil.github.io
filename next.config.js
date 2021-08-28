be3febfe4d42fa68890d27973140c454c247b9b7
Dalimil Hajek <dalimilhajek@gmail.com>
2021-08-28 16:03:59 -0400

    Set up packages

--- next.config.js
+++ next.config.js
@@ -1,4 +1,10 @@
+import path from 'path';
+
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
-}
+  sassOptions: {
+    // IncludePaths let's us directly @import 'mixins'; or @import 'variables'; anywhere without specifying full path
+    includePaths: [path.join(__dirname, 'src/styles')],
+  },
+};
