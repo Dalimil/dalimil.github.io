d59a65c1e134b9f5ea2fd08a2e57cfd14cae6eb2
Dalimil Hajek <dalimilhajek@gmail.com>
2021-08-30 19:22:15 -0400

    Add progress bar

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,4 +1,5 @@
 import type { AppProps } from 'next/app';
+import NextProgressBar from 'nextjs-progressbar';
 import { FC } from 'react';
 
 import { AppHead } from 'modules/head';
@@ -9,6 +10,7 @@ const MyApp: FC<AppProps> = ({ Component, pageProps }) => {
   return (
     <>
       <AppHead />
+      <NextProgressBar color="#29D" startPosition={0.3} stopDelayMs={200} height={3} />
       <Component {...pageProps} />
     </>
   );
